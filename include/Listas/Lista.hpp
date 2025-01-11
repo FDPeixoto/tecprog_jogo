f594dc0e47b199ceb0e07bdce47f2bd73a79450d
FDPeixoto <felipediaspeixoto@alunos.utfpr.edu.br>
2025-01-10 23:37:46 -0300

    Classe Lista e Elemento (Aninhada) completa

--- include/Listas/Lista.hpp
+++ include/Listas/Lista.hpp
@@ -1,5 +1,5 @@
-
 //O código do Giovani foi tulizado como inspiração: https://github.com/Giovanenero/JogoPlataforma2D-Jungle/blob/main/Jungle%2B%2B/include/Lista/Lista.hpp
+//O codigo do professor Jean Simao tambem foi utilizado como inspiracao
 #pragma once
 #include <iostream>
 
@@ -14,33 +14,79 @@ namespace Listas{
       private:
         TE* pInfo;
         Elemento<TE>* pProx;
+        Elemento<TE>* pAnte;
       public:
-        Elemento(): pInfo(nullptr), pProx(nullptr){}
-        ~Elemento();
-        void incluir(TE* p){
-          if(p != nullptr){pInfo = p;}
+        Elemento(): pInfo(nullptr), pProx(nullptr), pAnte(nullptr){}
+
+        Elemento(TE* info): pInfo(info), pProx(nullptr), pAnte(nullptr){}
+        
+        ~Elemento(){
+          pInfo = nullptr;
+          pProx = nullptr;
+          pAnte = nullptr;
         }
-        void setProx(Elemento<TE>* pE){
-          if(pE != nullptr){pProx = pE;}
-        }
-        const Elemento<TE>* getProximo(){
-          return pProx;
-        }
+
+        void setInfo(TE* p){pInfo = p;}
+        
+        TE* getInfo(){return pInfo;}
+
+        void setProx(Elemento<TE>* pP){pProx = pP;}
+        
+        const Elemento<TE>* getProx(){return pProx;}
+
+        void setAnte(Elemento<TE>* pA){}
+        
+        const Elemento<TE>* getAnte(){return pAnte;}
     };
 
-  Elemento<TL>* pAuxiliarPrimeiro; 
-  Elemento<TL>* pAuxiliarUltimo;   
-
   private:
-    Elemento<TL>* pPrimeiro, pUltimo;
+    Elemento<TL>* pPrimeiro;
+    Elemento<TL>* pAtual;
+    int tam;
+
   public:
-    /*
-    Lista();
-    ~Lista();
-    void incluir(TL* p);
-    void removerElemento(TL* elem);
-    void limpar();
-    void listar();
-    const int getTam(): */
+    Lista(): pPrimeiro(nullptr), pAtual(nullptr){tam = 0;}
+    
+    ~Lista(){
+      Elemento<TL>* aux = pPrimeiro;
+      while (aux != nullptr)
+      {
+        pPrimeiro = aux->getProx();
+        delete aux;
+        aux = pPrimeiro;
+      }
+    }
+
+    Elemento<TL>* getPrimeiro(){return pPrimeiro;}
+    
+    Elemento<TL>* getAtual(){return pAtual;}
+
+    Elemento<TL>* getTam(){return tam;}
+
+    void limpar(){
+      
+    }
+
+    void incluirElemento(TL* pElemento){
+      if (pElemento != nullptr)
+      {
+        Elemento<TL>* pAux = new Elemento<TL>();
+        pAux->setInfo(pElemento);
+        pAux->setProx(nullptr);
+        pAux->setAnte(nullptr);
+        if (pPrimeiro == nullptr)
+        {
+          pPrimeiro = pAux;
+          pAtual = pPrimeiro;		
+        }
+        else
+        {
+          pAtual->setProx( pAux);
+          pAux->setAnte( pAtual );
+          pAtual = pAux;
+        }
+        tam++;
+      }
+    }
   };
 }
